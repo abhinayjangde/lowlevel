91392690f0fb88f9e8c7ea3e9a55563ac213b6d3
Abhinay Jangde <abhinayjangde@gmail.com>
2026-09-21 12:40:07 +0530

    taking command line args

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
-int main(void)
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv)
 {
-    printf("Hello, World"); // this is hello /
+    if (argc < 2)
+    {
+        puts("Argument missing.");
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+
     return 0;
 }
